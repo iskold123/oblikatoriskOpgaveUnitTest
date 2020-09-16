05cee3c238b76e7a089a860c5fa7e9dd0ebf0124
Mads Olsen <mads36g6@edu.easj.dk>
2020-09-16 09:57:01 +0200

    Add project files.

---
+++ oblikatoriskOpgave/Cykel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oblikatoriskOpgave
+{
+    public class Cykel
+    {
+        #region Instanser
+
+        private int _id;
+        private string _color;
+        private int _price;
+        private int _gear;
+
+        #endregion
+
+        #region Property
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Color
+        {
+            get => _color;
+            set
+            {
+                if (value.Length < 1) throw new ArgumentOutOfRangeException();
+                _color = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0) throw new ArgumentNullException($"price cant be less then 0");
+                _price = value;
+            } 
+    }
+
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                if (value < 3 || value > 32 ) throw new ArgumentOutOfRangeException();
+                _gear = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Cykel()
+        {
+
+        }
+
+        public Cykel(int id, string color, int price, int gear)
+        {
+            _id = id;
+            _color = color;
+            _price = price;
+            _gear = gear;
+        }
+
+        #endregion
+
+        #region Metoder
+
+        public override string ToString()
+        {
+            return "";
+        }
+
+        #endregion
+
+    }
+}
---
+++ oblikatoriskOpgaveTests/CykelTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using oblikatoriskOpgave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oblikatoriskOpgave.Tests
+{
+    
+    [TestClass()]
+    public class CykelTests
+    {
+        private Cykel c1;
+
+        [TestInitialize]
+        public void Init()
+        {
+            c1 = new Cykel(1,"blue",1000,3);
+        }
+
+        [TestMethod()]
+        public void IdTest()
+        {
+            Assert.AreEqual(1, c1.Id);
+            c1.Id = 2;
+        }
+
+        [TestMethod()]
+        public void ColorTest()
+        {
+            Assert.AreEqual("blue", c1.Color);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Color = "");
+            c1.Color = "Red";
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual(1000,c1.Price);
+            Assert.ThrowsException<ArgumentNullException>(() => c1.Price = -1);
+            c1.Price = 100;
+
+        }
+
+        [TestMethod()]
+        public void GearTest()
+        {
+            Assert.AreEqual(3, c1.Gear);
+            c1.Gear = 16;
+            Assert.AreEqual(16,c1.Gear);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => c1.Gear = 33);
+
+        }
+    }
+}
